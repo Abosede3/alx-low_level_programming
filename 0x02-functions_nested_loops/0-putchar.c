6b95776c8a9e36ed6b8240874344f2e7d08841d5
Awoniyi Abosede <awoniy.abosede@gmail.com>
2022-03-17 06:30:27 -0700

    commiting

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,23 @@
-#include <stdio.h>
-	/* printing _putchar */
-char main(void)
-{	
-	char _putchar = "_putchar";
+#include "main.h"
 
-	printf(_putchar '\n');
+/**
+ * main - prints _putchar
+ *
+ * Description: prints _putchar
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char text[9] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+
 	return (0);
 }
