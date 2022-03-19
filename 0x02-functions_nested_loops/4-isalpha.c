673e5592717ddb6381f75d32f9e9ce3dc52fcb0e
Awoniyi Abosede <awoniy.abosede@gmail.com>
2022-03-19 13:30:16 -0700

    commiting _is-alpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+/**
+ * _isalpha - Entry point
+ * @c: c is an ascii character
+ *
+ * Return: returns 1 if c is lowercase or 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
