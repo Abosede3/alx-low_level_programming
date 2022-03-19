6fb79d03a5ea75ef8f55cac39f35f6982cd2418d
Awoniyi Abosede <awoniy.abosede@gmail.com>
2022-03-19 10:40:57 -0700

    outputing _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+
+/**
+ * main - print out putchar
+ *
+ * Description: function that print out _putchar
+ *
+ * Return: Always return 0 as success
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
