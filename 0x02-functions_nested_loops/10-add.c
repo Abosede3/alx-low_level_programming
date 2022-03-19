b94ea72be28be7eacd0a8c40fa34beeb81e337b9
Awoniyi Abosede <awoniy.abosede@gmail.com>
2022-03-19 16:01:09 -0700

    commiting

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+/**
+ * add - Adds two int togther
+ * @n1: takes a value
+ * @n2: takes another value
+ *
+ * Return: sum
+ */
+
+int add(int n1, int n2)
+{
+	int sum = n1 + n2;
+
+	return (sum);
+}
