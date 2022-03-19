33613a19c03561ed06055ff21aeae8015a4c7796
Awoniyi Abosede <awoniy.abosede@gmail.com>
2022-03-19 15:00:05 -0700

    commiting 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+/**
+ * _abs - Function absolute int
+ * @n: absolute value of int
+ *
+ * Return: Always return 0 as (success)
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
