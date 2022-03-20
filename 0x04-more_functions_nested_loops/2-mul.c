7f47d22970d893f16c1afbfe31e7caf83928c678
Awoniyi Abosede <awoniy.abosede@gmail.com>
2022-03-20 09:11:57 -0700

    commiting

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+/**
+ * mul - Function for multiplication
+ * @a: interger
+ * @b: integer
+ * Return: return value
+ */
+
+int mul(int a, int b)
+{
+	int multipy = a * b;
+
+	return (multipy);
+}
