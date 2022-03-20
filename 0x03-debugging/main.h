b881f38595b91b461596e849ac626ebb1a178c6b
Awoniyi Abosede <awoniy.abosede@gmail.com>
2022-03-20 08:04:28 -0700

    commiting

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,11 @@
-#ifindef _main_h_
-#define _main_h_
+#ifndef MAIN_H
+#define MAIN_H
 
-int _pâˆâˆositive_or_negative(int c);
-print _largest_number(int n);
+#include <stdio.h>
 
-print _print_remaining_days(int n);
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 
+#endif
